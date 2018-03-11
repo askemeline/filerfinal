7681b641b6bd0b162aea5b15f08ce8ff3eb4b856
IvanMiIosevic <ivan.milosevic@supinternet.fr>
2018-03-11 14:17:34 +0100

    modified Controller to use new functions

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -12,6 +12,9 @@ class MainController extends BaseController
         session_start();
         if (isset($_SESSION['u_id'])) {
             $data['session'] = $_SESSION;
+            $manager = new FileManager();
+            $result = $manager->showFiles();
+            $data['files'] = $result;
         }
         return $this->render('home.html.twig', $data);
     }
@@ -73,52 +76,33 @@ class MainController extends BaseController
         if (isset($_SESSION['u_id'])) {
             $data['session'] = $_SESSION;
             if (isset($_FILES['userfile'])) { // si l'input file nomme userfile est défini ou pas
-                if (!is_dir("uploads/" . $_SESSION['u_id'] . "/")) {
-                    mkdir("uploads/" . $_SESSION['u_id'] . "/", 0777, true) or die("couldn't mkdir");
+                $manager = new FileManager();
+                $uploaded = $manager->uploadFile($_SESSION,$_FILES['userfile']);
+                if ($uploaded === true){
+                    $this->redirectToRoute('home');
                 }
-                if (is_dir("uploads/" . $_SESSION['u_id'] . "/")) { // si le dossier existe
-                    $uploaddir = 'uploads/' . $_SESSION['u_id'] . '/'; //verifie si le dossier existe
-                    $tempName = $_FILES['userfile']['name'];
-                    $uploadfile = $uploaddir . $tempName;
-                    if (file_exists($uploadfile)) { // si le fichier existe deja
-                        $i = 1;
-                        while (file_exists($uploadfile)) {
-                            $name = "(" . $i . ")" . $tempName;
-                            $uploadfile = $uploaddir . $name;
-                            $i++;
-                        }
-                    } else {
-                        $name = $tempName;
-                        $uploadfile = $uploaddir . $name;
-                    }
-                    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-                        $manager = new FileManager();
-                        $manager->uploadFile($uploadfile,$_SESSION['u_id']);
-                        $this->redirectToRoute('home');
-                    } else {
-                        echo "failed";
-                    }
-                }
-            } else{
+            } else {
                 return $this->render('upload.html.twig', $data);
-            }  
+            }
         } else {
             $this->redirectToRoute('home');
         }
 
     }
-    public function downloadAction()
+    public function editAction()
     {
         session_start();
         if (isset($_SESSION['u_id'])) {
-            $data['session'] = $_SESSION;
-            if (isset($_POST['download'])) {
-                $path = $_SESSION['path'].$_POST['download'];
+            $selectedFile = str_replace('/', '', $_POST['secretName']);
+            if (isset($_POST['downloadButton'])) {
+                $path = $_SESSION['path'] . $_POST['secretName'];
                 $manager = new FileManager();
                 $manager->downloadFile($path);
+            } else if (isset($_POST['deleteButton'])) {
+            } else if (isset($_POST['renameButton'])) {
+            } else {
                 $this->redirectToRoute('home');
             }
-            return $this->render('download.html.twig', $data);
         } else {
             $this->redirectToRoute('home');
         }
